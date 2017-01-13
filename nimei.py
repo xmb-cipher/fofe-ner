60bc4c054521a643b9a2b9d61d2cf64bae476cc2
Mingbin Xu <mingbin_xu@apple.com>
2017-01-13 17:12:18 -0500

    add an ignore list

--- nimei.py
+++
@@ -1,7 +0,0 @@
-#!/eecs/research/asr/mingbin/python-workspace/hopeless/bin/python
-
-import tensorflow as tf
-self.session = tf.Session( config = tf.ConfigProto( gpu_options = tf.GPUOptions() ) )
-
-while True:
-	pass
